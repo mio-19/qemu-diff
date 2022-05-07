16d600837e3b12ed0cd6d20e10737e1e4a5a5781
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-05-07 13:42:37 +0000

    up

--- main.c
+++ main.c
@@ -6,21 +6,26 @@
 #define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET)
 
 void difftest_init(int port);
+
 void difftest_regcpy(void *dut, bool direction);
+
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction);
+
 void difftest_exec(uint64_t n);
 
 int main(int argc, char *argv[]) {
+    int port = 8888;
     // exit if the number of arguments is not 2
-    if (argc != 2) {
-        printf("Usage: ./main <port>\n");
+    if (argc == 2) {
+        // get port number from command line
+        port = atoi(argv[1]);
+    } else if (argc >= 2) {
+        printf("Usage: %s <port>\n", argv[0]);
         return 1;
     }
-    // get port number from command line
-    int port = atoi(argv[1]);
     // initialize difftest
     difftest_init(port);
-    difftest_memcpy(RESET_VECTOR, (void*) img, img_size, DIFFTEST_TO_REF);
+    difftest_memcpy(RESET_VECTOR, (void *) img, img_size, DIFFTEST_TO_REF);
 
     // run the program
     int c = instr_number;
